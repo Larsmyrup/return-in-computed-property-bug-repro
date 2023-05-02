879d713d89a35794b04b1f519705757a496b63e3
larsmyrup <hej@larsmyrup.dk>
2023-05-02 17:06:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Foo {
+  category: 'foo'
+}
+
+interface Baz {
+  category: 'baz'
+}
+
+type unionType = Foo | Baz
+
+//Use Math random so typescript can't infer the type
+const foo: unionType = Math.random() > 0.5 ? { category: 'foo' } : { category: 'baz' }
+
+const computedStuff = computed(() => {
+  switch (foo.category) {
+    case 'foo':
+      return 'foo'
+    case 'baz':
+      return 'baz'
+  }
+})
+
+</script>
+
+<template>
+  {{ computedStuff }}
+</template>
